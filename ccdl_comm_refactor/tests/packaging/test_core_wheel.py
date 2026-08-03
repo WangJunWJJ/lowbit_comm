d318c6088e84802a3a452b233a7ac7f1f9b2af6a
WangJunWJJ <wj_xd@foxmail.com>
2026-08-04 00:56:46 +0800

    test(build): support python310 packaging checks

--- ccdl_comm_refactor/tests/packaging/test_core_wheel.py
+++ ccdl_comm_refactor/tests/packaging/test_core_wheel.py
@@ -1,6 +1,9 @@
 from __future__ import annotations
 
-import tomllib
+try:
+    import tomllib
+except ModuleNotFoundError:  # pragma: no cover - exercised on Python 3.10
+    import tomli as tomllib
 
 import ccdl_comm
 
